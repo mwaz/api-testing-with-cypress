2738420c520cd29d4981035b3134e2f283f9eb46
Waweru <amby.mwaz@gmail.com>
2021-11-02 15:52:02 +0300

    fix: cypress version and failing test

--- cypress/integration/api-tests/todo.spec.js
+++ cypress/integration/api-tests/todo.spec.js
@@ -1,16 +1,17 @@
 
 describe('TODO api testing', () => {
-
+let todoItem;
     it('fetches Todo items - GET', () => {
         cy.request('/todos/').as('todoRequest');
         cy.get('@todoRequest').then(todos => {
             expect(todos.status).to.eq(200);
             assert.isArray(todos.body, 'Todos Response is an array')
+            todoItem = todos.body[0]._id
         });
     });
 
     it('deletes Todo items - DELETE', () => {
-        cy.request('DELETE', '/todos/9').as('todoRequest');
+        cy.request('DELETE', `/todos/${todoItem}`).as('todoRequest');
         cy.get('@todoRequest').then(todos => {
             expect(todos.status).to.eq(200);
             assert.isString(todos.body, 'todo deleted!')
